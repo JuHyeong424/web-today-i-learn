fe497f2f07320845067a1cfe7ff4d15ca0c06765
JuHyeong424 <wkdwngud963@gmail.com>
2026-03-18 16:30:21 +0900

    feat: 폼 작성 추가 기능

--- js/index.js
+++ js/index.js
@@ -4,9 +4,23 @@
 
 const tilForm = document.querySelector("#til-form");
 const tilList = document.querySelector("#til-list");
+const tilItem = tilList.querySelector(".til-item");
 
 tilForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
   // TODO: 입력값을 가져와서 새 TIL 항목을 만들어 목록에 추가하세요
+  const tilDate = document.getElementById("til-date").value;
+  const tilTitle = document.getElementById("til-title").value;
+  const tilContent = document.getElementById("til-content").value;
+
+  const newTilItem = tilItem.cloneNode(true);
+  
+  newTilItem.querySelector("time").textContent = tilDate;
+  newTilItem.querySelector("h3").textContent = tilTitle;
+  newTilItem.querySelector("p").textContent = tilContent;
+
+  tilList.prepend(newTilItem);
+
+  tilForm.reset();
 });
